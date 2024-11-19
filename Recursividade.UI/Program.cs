f65806083593f3a0bf69839bb1e4604defb4df33
carlo <carlo@PC-CARLOS>
2024-11-19 13:41:52 -0300

    Add project files.

---
+++ Recursividade.UI/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string diretorioInicial = @"C:\Estados";
+ListarDiretorios(diretorioInicial);
+
+Console.WriteLine("FIM");
+
+//funções
+void ListarDiretorios (string caminhoInicial)
+{
+    Console.WriteLine($"Diretório : {caminhoInicial}");
+
+    string[] subdiretorios = Directory.GetDirectories(caminhoInicial);
+
+    foreach (var subdiretorioAtual in subdiretorios)
+    {
+        ListarDiretorios(subdiretorioAtual);
+    }
+
+}
